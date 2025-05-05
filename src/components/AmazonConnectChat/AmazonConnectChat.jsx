12352a684c167b954fb4e419fb30d46da712a808
Andre Elian Alata Calle <u201913445@upc.edu.pe>
2025-05-05 11:34:37 -0500

    ultimo

--- src/components/AmazonConnectChat/AmazonConnectChat.jsx
+++ src/components/AmazonConnectChat/AmazonConnectChat.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
-
+import "./AmazonConnectChat.css"; // Asegúrate de tener un archivo CSS para estilos adicionales
 
 const AmazonConnectChat = () => {
   useEffect(() => {
     // Inyectar el script en el head
     (function(w, d, x, id){
       const s = d.createElement('script');
-      s.src = 'https://db08fjupg2abb.cloudfront.net/amazon-connect-chat-interface-client.js';
+      s.src = 'https://novasys.my.connect.aws/connectwidget/static/amazon-connect-chat-interface-client.js';
       s.async = true;
       s.id = id;
       d.getElementsByTagName('head')[0].appendChild(s);
@@ -20,6 +20,38 @@ const AmazonConnectChat = () => {
       closeChat: { color: '#ffffff', backgroundColor: '#0080ff' }
     });
     window.amazon_connect('snippetId', 'QVFJREFIZ2ZYaENvQWJCb1ZtYmxRNlFMNVJYMlhab3BKY1p0RnRGQ2pJNGhxdnBvemdHR3VmZkFaVXlvM1ZGUDEyRGdDUzk4QUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNRGpMeVh2Z1Y3K1dvSkRGNEFnRVFnQ3RnNVNQTGpJS3BSbXJsN2RjU2RXeVNRbHJONjZsSlBIZWlFdDA1ZWczeEc2S2ZBTkVibytUY0VQQXc6OmFHVzlVb3lmbXhLVHdyZWxySkhGdFRDS2dVWHJIMWt5UW5RR25jMUdOcTZBODhEWk1UYlcxWG5qVmZtZVdvN29HY01sakt2eUpyaUZ3RUVIUXU2ZksyTVlRRnFkNURCRWg0cFdJdG0veTZBRFBYWThvMnI5ZDh4aUtYOWxEU1FPbEdNRzhIQzBpQjhmN1JlbnFxUXhIYkt6SDcwUXlndz0=');
+    window.amazon_connect('customStyles', {
+      global: {
+        frameWidth: '400px',
+        frameHeight: '600px',
+        fontSize: '14px',
+        textColor: '#333333',
+        typeface: "'Segoe UI', sans-serif"
+      },
+      header: {
+        headerTextColor: '#ffffff',
+        headerBackgroundColor: '#ff2c2c'
+      },
+      transcript: {
+        widgetBackgroundColor: '#f9f9f9',
+        customerMessageTextColor: '#ffffff',
+        customerChatBubbleColor: '#ff5e5e',
+        agentMessageTextColor: '#000000',
+        agentChatBubbleColor: '#e1e1e1'
+      },
+      footer: {
+        footerBackgroundColor: '#ffffff',
+        buttonTextColor: '#ffffff',
+        buttonBackgroundColor: '#ff2c2c',
+        buttonFontSize: '14px'
+      },
+      logo: {
+        logoMaxHeight: '40px',
+        logoMaxWidth: '120px'
+      }
+    });
+    
+    
     window.amazon_connect('supportedMessagingContentTypes', [
       'text/plain',
       'text/markdown',
@@ -32,3 +64,10 @@ const AmazonConnectChat = () => {
 };
 
 export default AmazonConnectChat;
+
+
+
+
+
+
+
